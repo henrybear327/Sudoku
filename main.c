3e3639eb78f83cc5e15ba2b28677d6857fd7690b
BeMg <gccbg04538@gmail.com>
2015-06-07 10:01:41 +0000

    main

--- main.c
+++ main.c
@@ -1,9 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main()
-{
-	
+#define DEBUG printf("TEST\n");
 
-	return 0;
+typedef struct {
+    int row;
+    int column;
+    int bd[9][9];
+}board;
+
+int total=0;
+
+void check(board b){
+    board t;
+    
+}
+
+int main(){
+    board a;
+    for(int i=0; i<9; i++){
+        for(int j=0; j<9; j++){
+            scanf("%d",&a.bd[i][j]);
+        }
+    }
+    check(a);
+    return 0;
 }
